87fbf4e0d11a9f3137ff7b6bceaf6f8a9c56e7c4
pabiryukova <55852944+pabiryukova@users.noreply.github.com>
2020-06-01 15:03:27 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,47 @@
 import requests
-from pprint import pprint
-response = requests.get('http://127.0.0.1:5000/status')
-pprint(response.json())
-response = requests.get('http://127.0.0.1:5000/history')
-pprint(response.json())
-response = requests.post('http://127.0.0.1:5000/send', json = {'username': 'nick', 'password': '123', 'text':'hello'})
-pprint(response.json())
-response = requests.get('http://127.0.0.1:5000/history')
-pprint(response.json())
+import threading
+import time
+
+def printing(timestampp):
+    while True:
+        response = requests.get('http://127.0.0.1:5000/chat')
+        data = response.json()
+
+        for message in data['messages']:
+
+            if message['timestamp'] > timestampp:
+                if message['addressee'] in [username, 'all']:
+                    print(message['username'], 'to', message['addressee'], ':', message['text'])
+                    timestampp = message['timestamp']
+
+        time.sleep(1)
+
+    return timestampp
+
+
+print('Введите имя:')
+username = input()
+print('Введите пароль:')
+password = input()
+
+response = requests.post(
+    'http://127.0.0.1:5000/authorisation',
+    json={'username': username, 'password': password})
+
+print(response)
+
+while data['ok'] != 'success':
+    # пока сервер не сообщит, что логин и пароль верны, продолжаем попытки авторизации
+    print('Неверное имя или пароль. Возможно, вы пытаетесь зайти под именем другого пользователя.')
+    print('Повторно введите имя:')
+    username = input()
+    print('Повторно введите пароль:')
+    password = input()
+
+    response = requests.post(
+        'http://127.0.0.1:5000/authorisation',
+        json={'username': username, 'password': password})
+
+    data = response.json()
+
+x = threading.Thread(target=printing(0), daemon=True)
